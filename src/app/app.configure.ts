750c54431b3091fef9eb536df97ba7f767bfd8d7
phil chan <phillipchan1@gmail.com>
2020-03-04 14:37:36 -0800

    Be more functional

--- src/app/app.configure.ts
+++ src/app/app.configure.ts
@@ -1,9 +1,11 @@
-import feathers from '@feathersjs/feathers'
 import compress from 'compression'
-import helmet from 'helmet'
+import configuration from '@feathersjs/configuration'
 import cors from 'cors'
 import express, { notFound, rest, urlencoded, json } from '@feathersjs/express'
-import configuration from '@feathersjs/configuration'
+import feathers from '@feathersjs/feathers'
+import helmet from 'helmet'
+import _ from 'lodash'
+
 import { JetlyApp } from './app.types'
 import { configureDBConnections } from './app.db'
 import { configureServices } from './app.services'
@@ -20,8 +22,8 @@ export const configureJetly = (): JetlyApp => {
     app.use(urlencoded({ extended: true, limit: '50mb' }))
     app.configure(rest())
     app.use((req, res, next) => {
-        // @ts-ignore
-        req.feathers.headers = req.headers
+        _.set(req, 'feathers.headers', req.headers)
+
         next()
     })
     app.configure(configureDBConnections())
