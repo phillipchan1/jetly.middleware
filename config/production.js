955a03d1949d3380bd890620ca2d7a2f2619fe01
phil chan <phillipchan1@gmail.com>
2020-03-05 16:16:33 -0800

    Try to set up prod

--- config/production.js
+++ config/production.js
@@ -3,4 +3,14 @@ module.exports = {
     host: 'localhost',
     port: process.env.PORT,
     url: 'https://esi.sce.com',
+    mysql: {
+        client: 'mssql',
+        connection: {
+            host: 'localhost',
+            user: process.env.SQL_USER,
+            port: 1433,
+            password: process.env.SQL_PASS,
+            database: 'jetly',
+        },
+    },
 }
